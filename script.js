ef9488e5715c44994eb09765fbe8ac477bb4047e
matias <matiasbarrale22@gmail.com>
2024-10-24 13:29:26 -0300

    Actualización de js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const horarios = {
-    "A": {horario:"9am",medico: "Fernando", consultorio: "A"},
-    "B": {horario:"10am",medico: "Manuel", consultorio: "B"},
-    "C": {horario:"11am",medico: "Matias", consultorio: "C"},
+    "A": {horario: "9am", medico: "Fernando", consultorio: "A"},
+    "B": {horario: "10am", medico: "Manuel", consultorio: "B"},
+    "C": {horario: "11am", medico: "Matias", consultorio: "C"},
 };
 
 let horarioSeleccionado;
 
 while (!horarios[horarioSeleccionado]) {
-    horarioSeleccionado = prompt("Indiqué el horario para su consulta médica:\n-A 9am-Fernando\n-B 10am-Manuel\n-C 11am-Matias");
+    horarioSeleccionado = prompt("Indica el horario para tu consulta médica:\n-A 9am-Fernando\n-B 10am-Manuel\n-C 11am-Matias");
 
     if (!horarios[horarioSeleccionado]) {
-        alert("El horario seleccionado no está disponible. Intente de nuevo.");
+        alert("El horario seleccionado no está disponible. Intenta de nuevo.");
     }
 }
 
 console.log(`Médico: ${horarios[horarioSeleccionado].medico}`);
 console.log(`Consultorio: ${horarios[horarioSeleccionado].consultorio}`);
-console.log(`horario: ${horarios[horarioSeleccionado].horario}`);
+console.log(`Horario: ${horarios[horarioSeleccionado].horario}`);
 
 const cambiarMedico = prompt("¿Deseas cambiar de médico? (si/no)").toLowerCase();
 
 if (cambiarMedico === "si" || cambiarMedico === "sí") {
-    const nuevoMedico = prompt("indiqué un nuevo médico:\n-A Fernando\n-B Manuel\n-C Matias").toLowerCase();
+    const nuevoMedico = prompt("Indica un nuevo médico:\n-A Fernando\n-B Manuel\n-C Matias").toLowerCase();
 
     const horarioEncontrado = Object.keys(horarios).find(horario => horarios[horario].medico.toLowerCase() === nuevoMedico);
 
@@ -31,7 +31,26 @@ if (cambiarMedico === "si" || cambiarMedico === "sí") {
         alert(`Has cambiado al médico ${horarios[horarioSeleccionado].medico}.`);
     } else {
         console.log("El médico seleccionado no está disponible.");
-        alert("El médico seleccionado no está disponible. Intente de nuevo.");
+        alert("El médico seleccionado no está disponible. Intenta de nuevo.");
+    }
+}
+
+const cambiarHorario = prompt("¿Deseas cambiar el horario? (si/no)").toLowerCase();
+
+if (cambiarHorario === "si" || cambiarHorario === "sí") {
+    let nuevoHorarioSeleccionado;
+
+    while (!horarios[nuevoHorarioSeleccionado]) {
+        nuevoHorarioSeleccionado = prompt(
+            "Indica el nuevo horario para tu consulta médica:\n" +
+            "-A 9am-Fernando\n-B 10am-Manuel\n-C 11am-Matias"
+        );
+
+        if (!horarios[nuevoHorarioSeleccionado]) {
+            alert("El horario seleccionado no está disponible. Intenta de nuevo.");
+        } else {
+            horarioSeleccionado = nuevoHorarioSeleccionado;
+        }
     }
 }
 
